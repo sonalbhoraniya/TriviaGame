96840000e58d45b227fdd857ec1b3dba562cfc8f
Sonal Bhoraniya <bhoraniya@bhoraniya-macbookair.roam.corp.google.com>
2019-12-23 20:23:56 -0800

    create html shell and pseudocode javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    // declare initial variables, will need: 
+    // array of questions & answers
+    // generate random question from array 
+    // need to distinguish between wrong and correct answers (variable for correct answer?)
+
+    // set a timer to run for each question 
+
+    // function to generate & display a random question and answers 
+
+    // conditional statements
+
+    // if right answer is clicked, display congrats view, after a few seconds run function generate new question
+
+    // if wrong answer is clicked, display wrong answer view, after a few seconds run function generate new question
+
+    // if timer runs out before answer is clicked, display time out view, after a few seconds run function generate new question
+
+    // display overall game stats at end 
+
+
+})
